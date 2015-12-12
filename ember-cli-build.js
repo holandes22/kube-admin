94306ed00fb2002a5aff12dd25ee970103da0834
Pablo Klijnjan <pabloklijnjan@gmail.com>
2015-12-12 10:46:59 +0200

    Using bootstrap alpha2

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -6,6 +6,7 @@ module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
     // Add options here
   });
+  app.import('bower_components/bootstrap/dist/css/bootstrap.min.css');
 
   // Use `app.import` to add additional libraries to the generated
   // output files.
