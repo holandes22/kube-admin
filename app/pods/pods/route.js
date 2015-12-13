2c931f8ee70429b330bc327e17394836acb3241e
Pablo Klijnjan <pabloklijnjan@gmail.com>
2015-12-13 19:27:19 +0200

    Changed url hierarchy (under namespaces)

--- app/pods/pods/route.js
+++ app/pods/pods/route.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   kubeClient: Ember.inject.service(),
 
-  model() {
-    return this.get('kubeClient').findAll('pod');
+  model(params) {
+    return this.get('kubeClient').query('pod', { filter: { namespace: params.namespace }});
   }
 });
--- app/router.js
+++ app/router.js
@@ -6,17 +6,17 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.route('pods', function() {
+  this.route('namespaces', function() {
+    this.route('namespace', { path: ':namespace' });
+  });
+  this.route('pods', { path: 'namespaces/:namespace/pods' }, function() {
     this.route('pod', { path: ':name' });
   });
-  this.route('replication-controllers', function() {
+  this.route('replication-controllers', { path: 'namespaces/:namespace/replication-controllers' }, function() {
     this.route('replication-controller', { path: ':name' });
   });
-  this.route('services', function() {
+  this.route('services', { path: 'namespaces/:namespace/services' }, function() {
     this.route('service', { path: ':name' });
-  });
-  this.route('namespaces', function() {
-    this.route('namespace', { path: ':name' });
   });
 });
 
