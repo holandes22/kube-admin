d06906ae6e8294eb10dc78d217413a202d19b0eb
Pablo Klijnjan <pabloklijnjan@gmail.com>
2015-12-13 18:02:21 +0200

    Fixed empty model in rc

--- app/pods/namespaces/namespace/route.js
+++ app/pods/namespaces/namespace/route.js
@@ -1,9 +1,4 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  kubeClient: Ember.inject.service(),
-
-  model() {
-    return this.get('kubeClient').findAll('namespace');
-  }
 });
