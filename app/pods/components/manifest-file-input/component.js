5ca0fc061c4423a090345f1bd92cc9df863163d6
Pablo Klijnjan <pabloklijnjan@gmail.com>
2015-12-29 19:18:23 +0200

    Added features to manifest input component

--- app/pods/components/manifest-file-input/component.js
+++ app/pods/components/manifest-file-input/component.js
@@ -1,21 +1,59 @@
 import Ember from 'ember';
+import { validator, buildValidations } from 'ember-cp-validations';
 
-const { run: { bind } } = Ember;
+const {
+  run: {
+    bind
+  }
+} = Ember;
 
-export default Ember.Component.extend({
 
-  selectedFile: null,
+var Validations = buildValidations({
+  fileInfo: validator('presence', true)
+});
 
-  selectedFileName: 'Select JSON manifest file',
+// TODO:
+// - Handle very large files (maybe show a progress bar and allow cancel)
+// - Validate type is application/json and parses correctly
+// - Validate is correct kind
+// - Show form validation error (is empty)
+
+export default Ember.Component.extend(Validations, {
+
+  fileInfo: null,
+
+  errorMessage: null,
 
   didInsertElement() {
     this.$('input:file').on('change', bind(this, 'handleFileSelection'));
   },
 
   handleFileSelection(event) {
+    this.set('errorMessage', null);
     const file = event.target.files[0];
-    this.set('selectedFile', file);
-    this.set('selectedFileName', file.name);
+
+    this.readFile(file).then((fileInfo) => {
+      this.set('fileInfo', fileInfo);
+    }).catch((reason) => {
+      window.console.log('TODO: handle error', reason);
+    });
+  },
+
+  readFile(file) {
+    const reader = new window.FileReader();
+    reader.readAsText(file);
+    return new Ember.RSVP.Promise((resolve, reject) => {
+      reader.onload = function(event) {
+        resolve({
+          name: file.name,
+          type: file.type,
+          text: event.target.result
+        });
+      };
+      reader.onerror = function(error) {
+        reject({ event: 'onerror', error });
+      };
+    });
   },
 
   actions: {
@@ -25,13 +63,33 @@ export default Ember.Component.extend({
     },
 
     action() {
-      if (!!this.get('selectedFile')) {
-        this.get('attrs').action(this.get('selectedFile'));
+      this.validateSync({ on: ['fileInfo'] });
+      if (!this.get('validations.isValid')) {
+        this.set('errorMessage', this.get('validations.attrs.fileInfo.messages')[0]);
       } else {
-        window.console.log('TODO: show validation error');
+        const fileInfo = this.get('fileInfo'),
+              kind = this.get('kind');
+        let manifest = {},
+            errorMessage = null;
+        try {
+          manifest = JSON.parse(fileInfo.text);
+          if (!manifest.kind) {
+            errorMessage = 'Bad manifest file (No kind attribute)';
+          }
+          if (manifest.kind && manifest.kind !== kind) {
+            errorMessage = `Resource kind should be ${kind}`;
+          }
+        } catch(error) {
+          errorMessage = 'Selected file must be valid JSON';
+        }
+
+        if (!!errorMessage) {
+          this.set('errorMessage', errorMessage);
+        } else {
+          this.get('attrs').action(manifest);
+        }
       }
     }
-
   }
 
 });
--- tests/integration/pods/components/manifest-file-input/component-test.js
+++ tests/integration/pods/components/manifest-file-input/component-test.js
@@ -1,25 +1,95 @@
+import Ember from 'ember';
 import { moduleForComponent, test } from 'ember-qunit';
+import wait from 'ember-test-helpers/wait';
 import hbs from 'htmlbars-inline-precompile';
 
-moduleForComponent('file-picker', 'Integration | Component | file picker', {
+moduleForComponent('manifest-file-input', 'Integration | Component | manifest file input', {
   integration: true
 });
 
-test('it renders', function(assert) {
-  
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });" + EOL + EOL +
+test('it renders no actionLabel', function(assert) {
+  this.render(hbs`{{manifest-file-input}}`);
+  assert.equal(this.$().text().trim(), 'Go');
+});
 
-  this.render(hbs`{{file-picker}}`);
+test('it renders actionLabel', function(assert) {
+  this.render(hbs`{{manifest-file-input actionLabel="Blah"}}`);
+  assert.equal(this.$().text().trim(), 'Blah');
+});
 
-  assert.equal(this.$().text().trim(), '');
+test('it shows an error if no file and removes it if file is selected', function(assert) {
+  assert.expect(2);
+  this.render(hbs`{{manifest-file-input}}`);
+  this.$('[data-autoid=submit]').click();
+  assert.equal(
+    this.$('[data-autoid=error-message]').text(),
+    "This field can't be blank"
+  );
+  let blob = new window.Blob(['file content'], {type: 'text/plain'});
+  let file = new window.File([blob], 'name.txt');
 
-  // Template block usage:" + EOL +
-  this.render(hbs`
-    {{#file-picker}}
-      template block text
-    {{/file-picker}}
-  `);
+  let event = Ember.$.Event('change', { target: { files: [file] } });
+  this.$('input:file').trigger(event);
+  return wait().then(() => {
+    assert.equal(this.$('[data-autoid=error-message]').text(), '');
+  });
+});
 
-  assert.equal(this.$().text().trim(), 'template block text');
+test('it shows an error if cannot parse text as JSON', function(assert) {
+  assert.expect(1);
+  this.render(hbs`{{manifest-file-input}}`);
+  let blob = new window.Blob(['aa'], {type: 'application/json'});
+  let file = new window.File([blob], 'name.json');
+
+  let event = Ember.$.Event('change', { target: { files: [file] } });
+  this.$('input:file').trigger(event);
+  return wait().then(() => {
+    this.$('[data-autoid=submit]').click();
+    assert.equal(this.$('[data-autoid=error-message]').text(), 'Selected file must be valid JSON');
+  });
+});
+
+test('it shows an error if manifest has no kind', function(assert) {
+  assert.expect(1);
+  this.render(hbs`{{manifest-file-input kind="Pod"}}`);
+  let blob = new window.Blob(['{"apiVersion": "v1"}'], {type: 'application/json'});
+  let file = new window.File([blob], 'manifest.json');
+
+  let event = Ember.$.Event('change', { target: { files: [file] } });
+  this.$('input:file').trigger(event);
+  return wait().then(() => {
+    this.$('[data-autoid=submit]').click();
+    assert.equal(this.$('[data-autoid=error-message]').text(), 'Bad manifest file (No kind attribute)');
+  });
+});
+
+test('it shows an error if kind in manifest is not the specified', function(assert) {
+  assert.expect(1);
+  this.render(hbs`{{manifest-file-input kind="Fake"}}`);
+  let blob = new window.Blob(['{"kind": "Pod"}'], {type: 'application/json'});
+  let file = new window.File([blob], 'manifest.json');
+
+  let event = Ember.$.Event('change', { target: { files: [file] } });
+  this.$('input:file').trigger(event);
+  return wait().then(() => {
+    this.$('[data-autoid=submit]').click();
+    assert.equal(this.$('[data-autoid=error-message]').text(), 'Resource kind should be Fake');
+  });
+});
+
+test('it passes up the manifest', function(assert) {
+  const create = function(manifest) {
+    assert.equal(manifest.kind, 'Pod');
+  };
+  this.set('actions', { create });
+  this.render(hbs`{{manifest-file-input kind="Pod" action=(action "create")}}`);
+  let blob = new window.Blob(['{"kind": "Pod"}'], {type: 'application/json'});
+  let file = new window.File([blob], 'manifest.json');
+
+  let event = Ember.$.Event('change', { target: { files: [file] } });
+  this.$('input:file').trigger(event);
+  return wait().then(() => {
+    this.$('[data-autoid=submit]').click();
+  });
+
 });
