805b221c34adbe0418bcf14357a688d6d3ac864a
Pablo Klijnjan <pabloklijnjan@gmail.com>
2015-12-31 18:40:37 +0200

    Trivial: Added ember-flash-messages

--- config/environment.js
+++ config/environment.js
@@ -17,6 +17,11 @@ module.exports = function(environment) {
     APP: {
       // Here you can pass flags/options to your application instance
       // when it is created
+    },
+
+    flashMessageDefaults: {
+      timeout: 4000,
+      types: ['positive', 'negative']
     }
   };
 
