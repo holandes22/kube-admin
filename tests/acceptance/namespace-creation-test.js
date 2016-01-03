cf5069a0f3c94ccdb192a98afcfedf2ceddea870
Pablo Klijnjan <pabloklijnjan@gmail.com>
2016-01-03 13:30:48 +0200

    Trivial: Refactored acceptance test to use PageObject

--- tests/acceptance/namespace-creation-test.js
+++ tests/acceptance/namespace-creation-test.js
@@ -1,48 +1,48 @@
 import { test } from 'qunit';
 import moduleForAcceptance from 'kube-admin/tests/helpers/module-for-acceptance';
+import page from '../pages/namespace-creation';
 
 moduleForAcceptance('Acceptance | namespace creation');
 
 test('creating a namespace by manifest file', function(assert) {
   assert.expect(3);
-  visit('/namespaces');
   const name = 'fake',
-        content = `{"apiVersion": "v1", "kind": "Namespace", "metadata": {"name": "${name}"}}`;
-  triggerFileSelected('input:file', content);
-  click('[data-autoid=submit-file]');
+        content = `{
+          "apiVersion": "v1",
+          "kind": "Namespace",
+          "metadata": {"name": "${name}"}
+        }`;
 
+  page.createByFile(content);
   andThen(function() {
     assert.equal(currentURL(), '/namespaces');
     assert.equal(server.db.namespaces[0].metadata.name, name);
-    assert.equal(find('.ui.positive.message').text().trim(), 'Successfully created');
+    assert.equal(page.success(), 'Successfully created');
   });
 });
 
 test('creating a namespace by name', function(assert) {
   assert.expect(3);
-  visit('/namespaces');
   const name = 'fake';
-  fillIn('#namespace-name-input', name);
-  click('[data-autoid=submit-name]');
+  page.createByName(name);
 
   andThen(function() {
     assert.equal(currentURL(), '/namespaces');
     assert.equal(server.db.namespaces[0].metadata.name, name);
-    assert.equal(find('.ui.positive.message').text().trim(), 'Successfully created');
+    assert.equal(page.success(), 'Successfully created');
   });
 });
 
-test('creating a shows error if already exists', function(assert) {
+test('creating shows error if already exists', function(assert) {
   assert.expect(2);
   visit('/namespaces');
   const name = 'already-exists';
-  fillIn('#namespace-name-input', name);
-  click('[data-autoid=submit-name]');
+  page.createByName(name);
 
   andThen(function() {
     assert.equal(currentURL(), '/namespaces');
     const expected = `namespaces "${name}" already exists`;
-    assert.equal(find('.ui.negative.message').text().trim(), expected);
+    assert.equal(page.error(), expected);
   });
 });
 
