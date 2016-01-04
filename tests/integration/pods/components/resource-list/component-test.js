cf47c4133ebf4e4c66758b46870af2a4c57236b9
Pablo Klijnjan <pabloklijnjan@gmail.com>
2016-01-04 21:39:17 +0200

    gh-5 Added human from now date in resource list closes #15

--- tests/integration/pods/components/resource-list/component-test.js
+++ tests/integration/pods/components/resource-list/component-test.js
@@ -1,32 +1,42 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
-import makeResourceObjects from '../../../../helpers/make-resource-objects';
 
 
 moduleForComponent('resource-list', 'Integration | Component | resource list', {
   integration: true
 });
 
+const makeResourceObjects = function(total) {
+  let resources = [];
+  for (let i = 0; i < total; i++) {
+    const name = `name${i}`,
+          namespace = `namespace${i}`,
+          creationTimestamp = `2016-01-04T07:46:54.${i}Z`;
+    resources.push({ metadata: { name, namespace, creationTimestamp } });
+  }
+  return resources;
+};
+
 test('it renders empty', function(assert) {
   assert.expect(1);
-  const model = [];
-  this.set('model', model);
+  this.set('model', []);
   this.render(hbs`{{resource-list type='pod' resources=model}}`);
   const containers = this.$('[data-autoid^=container]');
   assert.equal(containers.length, 0);
 });
 
 test('it renders resource list', function(assert) {
-  assert.expect(5);
-  const model = makeResourceObjects(10);
+  assert.expect(6);
+  const model = makeResourceObjects(5);
   this.set('model', model);
   this.render(hbs`{{resource-list type='pod' resources=model}}`);
   const containers = this.$('[data-autoid^=container]');
-  assert.equal(containers.length, 10);
+  assert.equal(containers.length, 5);
   assert.equal(this.$('[data-autoid=name0]').text().trim(), 'name0');
+  assert.equal(this.$('[data-autoid=name4]').text().trim(), 'name4');
   assert.equal(this.$('[data-autoid=namespace0]').text().trim(), 'namespace0');
-  assert.equal(this.$('[data-autoid=timestamp0]').text().trim(), '0');
-  assert.equal(this.$('[data-autoid^=namespace]').length, 10);
+  assert.ok(this.$('[data-autoid=timestamp0]'));
+  assert.equal(this.$('[data-autoid^=namespace]').length, 5);
 });
 
 test('it does not render namespace if type namespace', function(assert) {
