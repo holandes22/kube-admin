2f3e20721d7292dd7a49407b54af760b896d6093
Pablo Klijnjan <pabloklijnjan@gmail.com>
2016-02-07 09:23:27 +0200

    gh-22 Added csp rule for image

--- config/environment.js
+++ config/environment.js
@@ -28,7 +28,7 @@ module.exports = function(environment) {
       'script-src':  ["'self'"],
       'font-src':    ["'self'", "https://fonts.gstatic.com", "data:"],
       'connect-src': ["'self'"],
-      'img-src':     ["'self'"],
+      'img-src':     ["'self'", "data:"],
       'style-src':   ["'self'", "http://fonts.googleapis.com"],
       'media-src':   ["'self'"]
     }
