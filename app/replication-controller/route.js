235c6a78ed3006b66e76de251ee96e5222723af7
Pablo Klijnjan <pabloklijnjan@gmail.com>
2016-02-08 18:12:53 +0200

    gh-18 Wiring up the scale action in router WIP

--- app/replication-controller/route.js
+++ app/replication-controller/route.js
@@ -6,5 +6,13 @@ export default Ember.Route.extend({
   model(params) {
     return this.get('kubeClient').findRecord(
         'replicationcontroller', params.namespace, params.name);
+  },
+
+  actions: {
+    scale(replicas) {
+      let spec = this.modelFor('replicationController');
+      window.console.log('Got replicas', replicas);
+      window.console.log('Old value for replicas', spec.status.replicas);
+    }
   }
 });
