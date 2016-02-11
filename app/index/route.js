86cae5605f11d5851ea7b676134bbb6d2738c2c7
Pablo Klijnjan <pabloklijnjan@gmail.com>
2016-02-11 11:30:16 +0200

    gh-8 Adding some stats for index page WIP

---
+++ app/index/route.js
@@ -0,0 +1,15 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+  kubeClient: Ember.inject.service(),
+
+  model() {
+    let client = this.get('kubeClient'),
+        promises = {
+          pods: client.findAll('pod'),
+          services: client.findAll('service'),
+          replicationcontrollers: client.findAll('replicationcontroller'),
+        };
+    return Ember.RSVP.hash(promises);
+  },
+});
