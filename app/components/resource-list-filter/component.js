6c636090a1a27d2a701c619a9145de309ba542f2
Pablo Klijnjan <pabloklijnjan@gmail.com>
2016-02-16 10:30:01 +0200

    gh-13 Using contextual components for filters
        - This allows for great flexibility allowing the user
          to decide where the visual elements go

--- app/components/resource-list-filter/component.js
+++ app/components/resource-list-filter/component.js
@@ -2,19 +2,10 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
 
-  options: Ember.computed('resources', function() {
-    let options = new Set();
-    Ember.$.each(this.get('resources'), (index, resource) => {
-      options.add(resource.metadata.namespace);
-    });
-    return Array.from(options);
-  }),
-
   actions: {
     changeFilters(selected) {
       this.set('filters', selected);
     },
-
     clearFilters() {
       this.set('filters', []);
     }
