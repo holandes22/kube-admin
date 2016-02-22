184af4b24cde8c08451d500d2ba850f005dbb171
Pablo Klijnjan <pabloklijnjan@gmail.com>
2016-02-22 11:59:02 +0200

    gh-8 Added test for usage-bar component

---
+++ tests/integration/components/usage-bar/component-test.js
@@ -0,0 +1,24 @@
+import { moduleForComponent, test } from 'ember-qunit';
+import hbs from 'htmlbars-inline-precompile';
+
+moduleForComponent('usage-bar', 'Integration | Component | usage bar', {
+  integration: true
+});
+
+test('it updates percentage', function(assert) {
+  this.set('percent', 12);
+
+  this.render(hbs`{{usage-bar percent=percent color='blue' label=fake}}`);
+
+  assert.equal(this.$('.progress').attr('data-percent'), 12);
+  assert.equal(this.$().text().trim(), '12%');
+  assert.ok(this.$('.progress').hasClass('blue'));
+  assert.notOk(this.$('.progress').hasClass('error'));
+
+  this.set('percent', 100);
+
+  assert.equal(this.$('.progress').attr('data-percent'), 100);
+  assert.ok(this.$('.progress').hasClass('error'));
+  assert.notOk(this.$('.progress').hasClass('blue'));
+
+});
