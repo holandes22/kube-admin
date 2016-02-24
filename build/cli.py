4546496d15f83512132f74f25e7ffcb4825e3554
Pablo Klijnjan <pabloklijnjan@gmail.com>
2016-02-24 22:18:59 +0200

    gh-4 and gh-2 Some cleanup in release script
        - Also added steps to travis config

--- build/cli.py
+++ build/cli.py
@@ -1,25 +1,20 @@
-# Requires jinja2, click and boto (or some s3 client)
-
-# Take from user
-# - version (default env)
-# - build_number (default '')
-
-# Usage:
-# - build: only builds image, generate kubernetes templates
-# - release: builds docker image, pushes to docker hub, deploys demo (dev) to s3
 import os
+import tarfile
 from subprocess import check_output
 
 import click
 from jinja2 import Environment, FileSystemLoader
 
-VERSION = 'dev'
-IMAGE = 'holandes22/kube-admin'
-
 
 def build_docker_image(image):
+    click.echo('Build docker image')
     cmd = ['docker', 'build', '-qt', image, '.']
     check_output(cmd)
+
+
+def push_docker_image(image):
+    click.echo('Pushing docker image')
+    cmd = ['docker', 'push', image]
 
 
 def generate_manifests(path, kwargs, out='kubernetes'):
@@ -32,21 +27,21 @@ def generate_manifests(path, kwargs, out='kubernetes'):
             f.write(template.render(**kwargs))
 
 
-def bundle_manifests():
-    pass
-
-
-def push_docker_image():
-    pass
+def bundle_kube_scripts(path, out='kubernetes.tar.gz'):
+    click.echo('Bundling kube script and manifests')
+    with tarfile.open(out, "w:gz") as tar:
+        tar.add(path, arcname='kube-admin')
 
 
 def build_ember(env='production', out='build/dist'):
+    click.echo('Building ember with env {}'.format(env))
     cmd = ['ember', 'build', '--environment', env, '--output-path', out]
     check_output(cmd)
 
 
-def deploy_demo():
-    pass
+def deploy():
+    click.echo('Deploying demo')
+    cmd = ['ember', 'deploy:dev']
 
 
 @click.group()
@@ -54,7 +49,7 @@ def deploy_demo():
 @click.option('--image', default='holandes22/kube-admin')
 @click.pass_context
 def cli(ctx, version, image):
-    ctx.obj['tpl_kwargs'] = {
+    ctx.obj['kwargs'] = {
         'version': version,
         'image': '{}:{}'.format(image, version)
     }
@@ -65,16 +60,23 @@ def cli(ctx, version, image):
 def build(ctx):
     click.echo('Building')
     build_ember()
-    build_docker_image(ctx.obj['tpl_kwargs']['image'])
+    build_docker_image(ctx.obj['kwargs']['image'])
 
 
 @cli.command()
 @click.pass_context
 def release(ctx):
     click.echo('Releasing')
-    generate_manifests('templates', ctx.obj['tpl_kwargs'])
+    image = ctx.obj['kwargs']['image']
+    #build_ember()
+    #build_docker_image(image)
+    #push_docker_image(image)
+
+    deploy()
+
+    generate_manifests('templates', ctx.obj['kwargs'])
+    bundle_kube_scripts('kubernetes')
 
 
 if __name__ == '__main__':
     cli(obj={})
-
